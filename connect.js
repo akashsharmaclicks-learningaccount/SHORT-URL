1e0752db0716b744272328c347c4f1cdae66f903
Akash Sharma <akash.sharma@igniteconnex.com>
2025-03-19 17:58:18 +0530

    Added comments

--- connect.js
+++ connect.js
@@ -1,8 +1,13 @@
+// Importing the Mongoose library for MongoDB interaction
 const mongoose = require("mongoose");
+
+// Function to establish a connection to MongoDB
+// Takes the MongoDB connection string (url) as a parameter
 async function connectToMongoDB(url) {
-  return mongoose.connect(url);
+  return mongoose.connect(url); // Connects to the MongoDB database using the provided URL
 }
 
+// Exporting the connectToMongoDB function for use in other files
 module.exports = {
   connectToMongoDB,
 };
